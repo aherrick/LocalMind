13de74126dc908affe75b79fda62e6f511c165ff
Andrew Herrick <aj@andrewherrick.com>
2026-08-18 18:48:16 -0400

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml;
+using Velopack;
+
+namespace LocalMind
+{
+    public partial class App : Application
+    {
+        private Window? _window;
+
+        public App()
+        {
+            VelopackApp.Build().Run();
+            InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            _window = new MainWindow();
+            _window.Activate();
+        }
+    }
+}
